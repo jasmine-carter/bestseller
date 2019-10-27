a77385319967c872595f4bbd6cea8c4d5e0dab79
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-27 22:31:30 +0000

    CLI class work

---
+++ lib/bestseller/cli.rb
@@ -0,0 +1,9 @@
+class BestSeller::CLI
+
+  def call
+    puts "Welcome to the New York Times Best Seller CLI!"
+    puts "Here are the current best seller categories."
+    #will list out N categories
+  end  
+  
+end 
