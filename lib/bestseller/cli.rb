46ddd3b17b69daa934b87ca342ae9e444cc06a9e
Jasmine Carter <heyjasminec@gmail.com>
2019-11-05 08:25:34 -0500

    Stubbed out improved menu input logic

--- lib/bestseller/cli.rb
+++ lib/bestseller/cli.rb
@@ -10,12 +10,27 @@ class BestSeller::CLI
   def menu
     puts "If you'd like to see books within a category, enter the category number:"
     input = ""
-    while input != "exit"
+    #while input != "exit"
       #case when input is an integer list_books_by_categories(input)
       #case when input is a string get_book_information
-      input = gets.chomp
-      list_books_by_categories(input)
-       puts "If you'd like to see more info on any of these books, enter book title. If you'd like to go back to categories, type categories, or type exit."
+      #input = gets.chomp
+      #list_books_by_categories(input)
+       #puts "If you'd like to see more info on any of these books, enter book title. If you'd like to go back to categories, type categories, or type exit."
+
+       #revised case statement for input handling
+       input = gets.chomp
+       case input
+       when "exit"
+         goodbye
+       when input.downcase.include?("category")
+         list_books_by_categories(input[-1].to_i)
+       when input.downcase.include?("book")
+         #show selected book get_book_information
+       when input == "categories"
+         list_categories
+       else
+        puts "Command not recognized. Type \"categories\" to view a list of categories.\n Type exit to end."
+
     end
   end
 
@@ -43,7 +58,7 @@ class BestSeller::CLI
 
   def selected_book(book_name) #this method finds the book the use selected from Books.all
     Books.all.select do |b|
-      b.name == book_name
+      b.title == book_name
     end
   end
 
