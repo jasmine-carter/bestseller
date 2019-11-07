0349ee5c8ee5df6b75c5b491a645175b0b53f3ea
Jasmine Carter <heyjasminec@gmail.com>
2019-11-06 20:42:26 -0500

    built out menu case statement, removed old comments

--- lib/bestseller/cli.rb
+++ lib/bestseller/cli.rb
@@ -7,50 +7,46 @@ class BestSeller::CLI
   end
 
   def menu
-    while input != "exit"
-    list_categories
-    state = category_view
-    #while in category view - prompt user to select category or exit
-    #while in books by category view - prompt user to select a book, exit, or go back to category view
-    #while in book view - prompt user to exit, go back to books by that category, or categories views
-    puts "If you'd like to see books within a category, enter the category number:"
-    input = ""
-      #case when input is an integer list_books_by_categories(input)
-      #case when input is a string get_book_information
-      input = gets.chomp
-      list_books_by_categories(input) #turn into find or create list by category
-        back = input #allows user to call list of categories from book view
-        state = books_by_category_view
-       puts "If you'd like to see more info on any of these books, enter book number. If you'd like to go back to categories, type categories, or type exit."
-          input = gets.chomp
-          if input.to_i != 0
-            state = book_view
-          elsif input == "categories"
-            state = category_view
-          elsif input == "exit"
-            goodbye
-          elsif input == "back"
-            list_books_by_categories(back)
-            state = books_by_category_view
-          end
-       #revised case statement for input handling
-       #input = gets.chomp
-       #case input
-       #when "exit"
-        # goodbye
-       #when input.downcase.include?("category")
-         #list_books_by_categories(input[-1].to_i)
-       #when input.downcase.include?("book")
-         #example input "book 2"
-         #takes the input, strips to number of the book
-         #need to get from string input of book 2, to be a book title for show_selected_book_info method
-         #self.show_selected_book_info(input)
-       #when input == "categories"
-         #list_categories
-       #else
-        #puts "Command not recognized. Type \"categories\" to view a list of categories.\n Type exit to end."
-
-    end
+      input = ""
+      state = "category_view"
+      #list_categories
+      #input = gets.chomp
+    while input.downcase != "exit"
+    	case state
+      when "category_view"
+      		list_categories
+      		puts "If you'd like to see books within a category, enter the category number:"
+      		input = gets.chomp
+            	list_books_by_categories(input)
+            	back = input
+            	state = "books_by_category_view"
+          when "books_by_category_view"
+          	puts "If you'd like to see more info on any of these books, enter book number. If you'd like to go back to categories, type \"categories\", or type \"exit\"."
+                input = gets.chomp
+                if input.to_i != 0
+                 state = "book_view"
+                 puts "you're in the book view"
+               elsif input == "categories"
+                 state = "category_view"
+               elsif input == "back"
+                 list_books_by_categories(back)
+                 state = "books_by_category_view"
+                 puts "If you'd like to see more info on any of these books, enter book number. If you'd like to go back to categories, type categories, or type exit."
+               else
+                  puts "Command not recognized. \n If you'd like to see more info on any of these books, enter book number. If you'd like to go back to categories, type \"categories\", or type \"exit\"."
+                 input = gets.chomp
+               end
+          when "book_view"
+          	puts "If you'd like to go back to the list of books by category, type \"back\". \n To view the category list, type \"categories\".\n Or type \"exit\"."
+          	if input.downcase == "categories"
+          		state = "category_view"
+          	elsif input.downcase == "back"
+          		state = "books_by_category_view"
+          	else
+          	 puts "Command not recognized. To view the category list, type \"categories\".\n Or type \"exit\"."
+           end
+        end
+      end
   end
 
   def list_categories
